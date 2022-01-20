d8846c08e4f3c75b3149fa716d4011775e8654a5
daeho kim <daeho.kim@softwareinlife.com>
2022-01-20 20:01:35 +0900

    first commit

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
